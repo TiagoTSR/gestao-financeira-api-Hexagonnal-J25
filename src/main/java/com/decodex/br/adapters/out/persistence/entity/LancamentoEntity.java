33cce61180e68cc0055827ccc402efd6ffca7bb5
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-05-06 15:28:58 -0300

    feat: adiciona entidades JPA para persistência (PessoaEntity e CategoriaEntity e LancamentoEntity)

---
+++ src/main/java/com/decodex/br/adapters/out/persistence/entity/LancamentoEntity.java
@@ -0,0 +1,112 @@
+package com.decodex.br.adapters.out.persistence.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.decodex.br.domain.model.Categoria;
+import com.decodex.br.domain.model.Pessoa;
+import com.decodex.br.domain.model.TipoLancamento;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Lancamento")
+public class LancamentoEntity {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+    private String descricao;
+
+    private LocalDate dataVencimento;
+
+    private LocalDate dataPagamento;
+
+    private BigDecimal valor;
+
+    private String observacao;
+
+    private TipoLancamento tipo;
+
+    private Categoria categoria;
+
+    private Pessoa pessoa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(LocalDate dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public LocalDate getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(LocalDate dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public TipoLancamento getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoLancamento tipo) {
+		this.tipo = tipo;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+    
+}
