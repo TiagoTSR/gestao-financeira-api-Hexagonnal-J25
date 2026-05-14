0c88c06c7f6d0d4f5b81672cca3cff6d09e74a7d
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-05-14 08:45:57 -0300

    fix: adicionar anotações de validação nos DTOs de Pessoa e Categoria

--- src/main/java/com/decodex/br/application/dto/categoria/CategoriaUpdateDTO.java
+++ src/main/java/com/decodex/br/application/dto/categoria/CategoriaUpdateDTO.java
@@ -1,6 +1,11 @@
 package com.decodex.br.application.dto.categoria;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CategoriaUpdateDTO(
 		
+		@NotBlank
+        @Size(min = 3, max = 50)
 		String nome
 ) {}
--- src/main/java/com/decodex/br/application/dto/pessoa/PessoaUpdateDTO.java
+++ src/main/java/com/decodex/br/application/dto/pessoa/PessoaUpdateDTO.java
@@ -2,12 +2,17 @@ package com.decodex.br.application.dto.pessoa;
 
 import com.decodex.br.domain.model.Endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record PessoaUpdateDTO(
 		
+		@NotBlank
 	    String nome,
 
 	    Endereco endereco,
-
+	    
+	    @NotNull
 	    Boolean ativo
 
 ) {}
