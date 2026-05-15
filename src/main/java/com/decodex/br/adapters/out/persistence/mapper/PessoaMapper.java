fbbb0325c0de7f072c8149ca0d9c2cbbf43ddcd7
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-05-15 15:39:24 -0300

    refactor(persistence): atualizar PessoaMapper para mapear EnderecoEmbeddable

--- src/main/java/com/decodex/br/adapters/out/persistence/mapper/PessoaMapper.java
+++ src/main/java/com/decodex/br/adapters/out/persistence/mapper/PessoaMapper.java
@@ -1,26 +1,52 @@
 package com.decodex.br.adapters.out.persistence.mapper;
 
+import com.decodex.br.adapters.out.persistence.entity.EnderecoEmbeddable;
 import com.decodex.br.adapters.out.persistence.entity.PessoaEntity;
+import com.decodex.br.domain.model.Endereco;
 import com.decodex.br.domain.model.Pessoa;
 
 public class PessoaMapper {
- public Pessoa toDomain(PessoaEntity entity) {
-     if (entity == null) return null;
-     return new Pessoa(
-         entity.getId(),
-         entity.getNome(),
-         entity.getEndereco(),
-         entity.getAtivo()
-     );
- }
 
- public PessoaEntity toEntity(Pessoa pessoa) {
-     if (pessoa == null) return null;
-     PessoaEntity entity = new PessoaEntity();
-     entity.setId(pessoa.getId());
-     entity.setNome(pessoa.getNome());
-     entity.setEndereco(pessoa.getEndereco());
-     entity.setAtivo(pessoa.getAtivo());
-     return entity;
- }
+    public Pessoa toDomain(PessoaEntity entity) {
+        if (entity == null) return null;
+
+        Endereco endereco = new Endereco(
+            entity.getEndereco().getLogradouro(),
+            entity.getEndereco().getNumero(),
+            entity.getEndereco().getComplemento(),
+            entity.getEndereco().getBairro(),
+            entity.getEndereco().getCep(),
+            entity.getEndereco().getCidade(),
+            entity.getEndereco().getEstado()
+        );
+
+        return new Pessoa(
+            entity.getId(),
+            entity.getNome(),
+            endereco,
+            entity.getAtivo()
+        );
+    }
+
+    public PessoaEntity toEntity(Pessoa pessoa) {
+        if (pessoa == null) return null;
+
+        EnderecoEmbeddable enderecoEmbeddable = new EnderecoEmbeddable();
+        enderecoEmbeddable.setLogradouro(pessoa.getEndereco().getLogradouro());
+        enderecoEmbeddable.setNumero(pessoa.getEndereco().getNumero());
+        enderecoEmbeddable.setComplemento(pessoa.getEndereco().getComplemento());
+        enderecoEmbeddable.setBairro(pessoa.getEndereco().getBairro());
+        enderecoEmbeddable.setCep(pessoa.getEndereco().getCep());
+        enderecoEmbeddable.setCidade(pessoa.getEndereco().getCidade());
+        enderecoEmbeddable.setEstado(pessoa.getEndereco().getEstado());
+        
+
+        PessoaEntity entity = new PessoaEntity();
+        entity.setId(pessoa.getId());
+        entity.setNome(pessoa.getNome());
+        entity.setEndereco(enderecoEmbeddable);
+        entity.setAtivo(pessoa.getAtivo());
+
+        return entity;
+    }
 }
