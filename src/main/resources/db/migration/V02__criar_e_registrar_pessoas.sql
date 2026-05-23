f0e465fcc71acdb6da447c83498156d55fe09293
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-05-23 09:53:33 -0300

    refactor(db): atualizar restrições de esquema e tamanhos de colunas de Pessoa

--- src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V02__criar_e_registrar_pessoas.sql
@@ -1,13 +1,13 @@
 CREATE TABLE pessoa (
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    logradouro VARCHAR(30),
-    numero VARCHAR(30),
-    complemento VARCHAR(30),
-    bairro VARCHAR(30),
-    cep VARCHAR(30),
-    cidade VARCHAR(30),
-    estado VARCHAR(30),
+    logradouro VARCHAR(100),
+    numero VARCHAR(20),
+    complemento VARCHAR(60),
+    bairro VARCHAR(60),
+    cep VARCHAR(10),
+    cidade VARCHAR(60),
+    estado VARCHAR(2),
     ativo BOOLEAN NOT NULL
 );
 
