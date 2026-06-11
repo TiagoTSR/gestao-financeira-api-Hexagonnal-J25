1e45869b056e354000917077d62c92442b4bbe3e
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-06-11 16:19:46 -0300

    feat(config): inicializa o dotenv para carregar variáveis ​​de ambiente na inicialização

--- src/main/java/com/decodex/br/GestaoFinanceiraApiHexagonalJ25Application.java
+++ src/main/java/com/decodex/br/GestaoFinanceiraApiHexagonalJ25Application.java
@@ -3,10 +3,17 @@ package com.decodex.br;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class GestaoFinanceiraApiHexagonalJ25Application {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(GestaoFinanceiraApiHexagonalJ25Application.class, args);
 	}
 
