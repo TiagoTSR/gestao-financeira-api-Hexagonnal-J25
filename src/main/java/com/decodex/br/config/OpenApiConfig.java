81636e9b2c8fe4c18c65678706d2c76330b50953
TIAGO SILVA RUFINIANO <78699713+TiagoTSR@users.noreply.github.com>
2026-08-07 12:12:08 -0300

    feat(config): adicionar classe OpenApiConfig para Swagger

---
+++ src/main/java/com/decodex/br/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package com.decodex.br.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Gestão Financeira API")
+                        .version("v0.0.1")
+                        .description("API para controle financeiro pessoal, desenvolvida com arquitetura hexagonal no Java 25.")
+                        .contact(new Contact()
+                                .name("Suporte")
+                                .email("suporte@decodex.com")));
+    }
+}
